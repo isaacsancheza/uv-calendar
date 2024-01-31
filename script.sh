ad994cd031d94a36bf64de95ef945ef3244ce5b4
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2024-01-31 11:32:27 -0600

    :technologist: update launch script

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+image_tag=scraper
+docker build -t $image_tag app
+
 docker run --rm -it \
     -e BUCKET_NAME=$BUCKET_NAME \
     -e CALENDARS_BUCKET_NAME=$CALENDARS_BUCKET_NAME \
-    -e AWS_REGION=$AWS_REGION \
+    -e AWS_DEFAULT_REGION=$REGION \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
-    uv
+    $image_tag
